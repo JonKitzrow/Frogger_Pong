dc4dfa10fc74b61e912f664bda93ce37a9b3d2da
Jonathan Kitzrow <jonkitzrow@gmail.com>
2019-06-17 23:25:31 -0500

    Pongball with border-bounce
    Sprite added for PongBall and bounce colliders surrounding the screen borders. Ball initializes with a randomized velocity.

---
+++ Assets/Scripts/PongBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongBall : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float accel, maxVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = new Vector3(Random.Range(0.5f, 5f), Random.Range(0.5f, 5f), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (rb.velocity.magnitude < maxVelocity)
+      {
+        rb.velocity *= 1 + (accel/100f) * Time.deltaTime;
+      }
+    }
+}
